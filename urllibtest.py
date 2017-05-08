f05baa67771452dec92b99385b204a7a31577d58
dongxiaobai <523862073@qq.com>
2017-05-08 17:41:40 +0800

    add files

---
+++ urllibtest.py
@@ -0,0 +1,8 @@
+from urllib import request
+
+with request.urlopen('https://www.baidu.com') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', data.decode('utf-8'))
