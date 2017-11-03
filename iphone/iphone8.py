440c1cdbb638d50b1c2d2025eff313372badd0a4
dongxiaobai <523862073@qq.com>
2017-11-03 14:11:47 +0800

    aa

--- iphone/iphone8.py
+++ iphone/iphone8.py
@@ -2,8 +2,8 @@ import re
 import pandas as pd
 import requests
 
-f = open('comment_iphone8.txt', 'a')
-for i in range(0,720):#720
+f = open('comment_iphone8_2.txt', 'a')
+for i in range(0,10):#10
     try:
         response = requests.get('https://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv4962&productId=5089225&score=0&sortType=5&page='+str(i)+'&pageSize=10&isShadowSku=0&rid=0&fold=1')
         response = response.text
--- iphone/iphone8cloud.py
+++ iphone/iphone8cloud.py
@@ -1,15 +1,17 @@
 from os import path
 from scipy.misc import imread
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import jieba
 from wordcloud import WordCloud
 
-f = open('comment_iphone8.txt','r')
+f=open('comment_iphone8_2.txt','r')
 text = f.read()
 
 cut_text = ' '.join(jieba.lcut(text))
 #print(cut_text)
-color_mask = imread("iphone8.jpg")
+color_mask = imread("iphone8pro.jpg")
 cloud = WordCloud(
     font_path='FZMWFont.ttf',
     background_color='white',
@@ -21,4 +23,4 @@ word_cloud = cloud.generate(cut_text)
 plt.imshow(word_cloud)
 plt.axis('off')
 plt.show()
-
+plt.savefig('iphone8cloud_2.png')
