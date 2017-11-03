b37f20741898252f21a6fe8f0f1c4dea1c307034
dongxiaobai <523862073@qq.com>
2017-11-03 12:21:57 +0800

    add

---
+++ iphone/iphone8.py
@@ -0,0 +1,21 @@
+import re
+import pandas as pd
+import requests
+
+f = open('comment_iphone8.txt', 'a')
+for i in range(0,720):#720
+    try:
+        response = requests.get('https://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv4962&productId=5089225&score=0&sortType=5&page='+str(i)+'&pageSize=10&isShadowSku=0&rid=0&fold=1')
+        response = response.text
+        #print(response)
+        pat = '"content":"(.*?)","'
+        res = re.findall(pat,response)
+        for i in res:
+            i = i.replace('\\n', '')
+            #print(i)
+            f.write(i)
+            f.write('\n')
+    except:
+        print('爬取第'+str(i)+'页出现问题')
+        continue
+f.close()
---
+++ iphone/iphone8cloud.py
@@ -0,0 +1,23 @@
+from os import path
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+import jieba
+from wordcloud import WordCloud
+
+f.open('comment_iphone8_.txt','r')
+text = f.read()
+cu_text = ' '.join(jieba.lcut(text))
+print(cut_text)
+color_mask = imread("iphone8.jpg")
+cloud = WordCloud(
+    font_path='FZMWFont.ttf',
+    background_color='white',
+    mask=color_mask,
+    max_words=200,
+    max_font_size=5000
+)
+word_cloud = cloud.generate(cut_text)
+plt.imshow(word_cloud)
+plt.axis('off')
+plt.show()
+
